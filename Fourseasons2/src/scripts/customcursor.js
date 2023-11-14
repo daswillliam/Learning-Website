a76172f414c635f2ab23ffbde25e355654a874a2
daswillliam <liam.rump@icloud.com>
2023-11-14 16:38:34 +0100

    Bug fixes and new look

--- Fourseasons2/src/scripts/customcursor.js
+++ Fourseasons2/src/scripts/customcursor.js
@@ -1,5 +1,6 @@
 const cursorDot = document.getElementById('cursor-dot');
 const cursorOutline = document.getElementById('cursor-outline');
+const cursorBall = document.getElementById('cursor-ball')
 
 
 window.addEventListener("mousemove", function (e) {
@@ -13,25 +14,46 @@ window.addEventListener("mousemove", function (e) {
     left: `${posX}px`,
     top: `${posY}px`
   }, {duration: 500, fill: "forwards"})
+
+  cursorBall.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+  }, {duration: 250, fill: "forwards"})
 });
 
 window.addEventListener("mousedown", function (e) {
     if (e.button === 0) { // 0 steht für den linken Mausklick
-        cursorOutline.classList.add('clicked');
+        cursorOutline.classList.add('clicked-outline');
+        cursorBall.classList.add('clicked-ball')
     }
 });
 
 window.addEventListener("mouseup", function () {
-    cursorOutline.classList.remove('clicked');
+    cursorOutline.classList.remove('clicked-outline');
+    cursorBall.classList.remove('clicked-ball')
 });
 
-const buttons = document.querySelectorAll('button, a, label, input, span');  // oder ein anderer Selektor für die gewünschten Elemente
+const buttons = document.querySelectorAll('button, a, label, input');  // oder ein anderer Selektor für die gewünschten Elemente
 
 buttons.forEach(button => {
   button.addEventListener('mouseenter', () => {
-      cursorOutline.classList.add('hovered');
+    cursorDot.classList.add('hovered-dot');
   });
   button.addEventListener('mouseleave', () => {
-      cursorOutline.classList.remove('hovered');
+    cursorDot.classList.remove('hovered-dot');
+  });
+  
+  button.addEventListener('mouseenter', () => {
+    cursorBall.classList.add('hovered-ball');
+  });
+  button.addEventListener('mouseleave', () => {
+    cursorBall.classList.remove('hovered-ball');
+  });
+  
+  button.addEventListener('mouseenter', () => {
+    cursorOutline.classList.add('hovered-outline');
+  });
+  button.addEventListener('mouseleave', () => {
+    cursorOutline.classList.remove('hovered-outline');
   });
 });
